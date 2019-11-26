a479e8b7368450aa1649bb6911d054e5441a1a17
Andres Mejias <aemabit@gmail.com>
2019-11-25 23:06:09 -0500

    create network styleseets and consulting data

--- app/javascript/bundles/Network/components/Network.jsx
+++ app/javascript/bundles/Network/components/Network.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import axios from 'axios'
+
 
 export default class Network extends React.Component{
+    state = {
+        myNetwork: []
+    }
+    
+
+    neighborhood = () => {
+        axios.get(`/fpl_accounts.json`)
+            .then(res => this.setState({ myNetwork: res.data}))
+    }
+    
     render(){
         return(
-            <h1>Hello world from Network</h1>
+        <div className="network-wrapper">
+            {
+                this.state.myNetwork.map((person, i) => {
+                    return(
+                        <div
+                        className="person-profile"
+                        key={i}
+                        id={i}
+                        >
+                            <p>{person.zipcode}</p>
+                            <p>{person.user_email}</p>
+                            <p>{person.points}</p>                        
+                        </div>
+                    )
+                })
+            }
+        </div>
         )
     }
+
+    componentDidMount(){
+        this.neighborhood()
+    }
+
 }
