d6ff73f8b24a93cbed9c445ea988755b46709f5b
Andres Mejias <aemabit@gmail.com>
2019-11-26 15:12:56 -0500

    add styles to network page

--- app/javascript/bundles/Network/components/Network.jsx
+++ app/javascript/bundles/Network/components/Network.jsx
@@ -15,23 +15,33 @@ export default class Network extends React.Component{
     
     render(){
         return(
-        <div className="network-wrapper">
-            {
-                this.state.myNetwork.map((person, i) => {
-                    return(
-                        <div
-                        className="person-profile"
-                        key={i}
-                        id={i}
-                        >
-                            <p>{person.zipcode}</p>
-                            <p>{person.user_email}</p>
-                            <p>{person.points}</p>                        
-                        </div>
-                    )
-                })
-            }
-        </div>
+            <>
+            <div>
+                <h1>My Neighborhood</h1>
+                <hr></hr>
+            </div>
+            <div className="container-network">
+                <div className="network-wrapper">
+                    {
+                        this.state.myNetwork.map((person, i) => {
+                            return(
+                                <div
+                                className="person-profile"
+                                key={i}
+                                id={i}
+                                >
+                                    <div className="network-person-avatar"></div>
+                                    <div>
+                                        <p className="person-points">{person.points}</p>                        
+                                        <p>{person.user_email}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            </>
         )
     }
 
